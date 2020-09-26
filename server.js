50b520539306eb2758f2f4abe7303ac0c9a7ca77
8bitk9 <67807672+dallisconner@users.noreply.github.com>
2020-09-26 12:53:07 -0400

    Various attempted changes to functionality

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const path = require("path");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+var reservations = [];
+
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
 });
@@ -17,6 +19,26 @@ app.get("/makereservation", function (req, res) {
 
 app.get("/viewtables", function (req, res) {
     res.sendFile(path.join(__dirname, "viewtables.html"));
+});
+
+app.get("/api/reservations", function (req, res) {
+    return res.json(reservations);
+});
+
+app.post("/api/reservations", function (req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body parsing middleware
+    var newReservation = req.body;
+
+    // Using a RegEx Pattern to remove spaces from newCharacter
+    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+    newReservation.routeName = newReservation.name.replace(/\s+/g, "").toLowerCase();
+
+    console.log(newReservation);
+
+    characters.push(newReservation);
+
+    res.json(newReservation);
 });
 
 app.listen(port, () => {
