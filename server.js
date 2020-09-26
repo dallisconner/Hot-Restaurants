d3054d968bd48650f60747895b77852ab1c59918
Soua Xiong <68033651+sxiong6901@users.noreply.github.com>
2020-09-26 11:24:19 -0400

    Update

--- server.js
+++ server.js
@@ -14,9 +14,6 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
 
-var app = express();
-var PORT = process.env.PORT || 3000;
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -30,7 +27,7 @@ var visitorCount = 0;
 
 // Routing
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "home.html"));
   visitorCount++;
 });
 
