af10ed9ad0d49dccfd9e94717696581dbd4c737f
Caroline Manson <carolinemanson15@gmail.com>
2020-09-26 12:19:43 -0400

    made lots of changes

--- server.js
+++ server.js
@@ -3,8 +3,43 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hot Restaurants')
-})
+  res.sendFile(path.join(__dirname, "index.html")
+  )
+});
+
+app.get("/add", function(req, res) {
+  res.sendFile(path.join(__dirname, "viewtables.html"));
+});
+
+app.get("/api/reservations", function(req, res) {
+  return res.json(reservations);
+});
+
+app.get("/api/reservations/:reservation", function(req, res) {
+  var chosen = req.params.reservations;
+
+  console.log(chosen);
+
+  for (var i = 0; i < reservations.length; i++) {
+    if (chosen === reservations[i].routeName) {
+      return res.json(reservations[i]);
+    }
+  }
+
+  return res.json(false);
+});
+
+app.post("/api/reservations", function(req, res) {
+  var newReservation = req.body;
+
+  newReservation.routeName = newReservation.name.replace(/\s+/g, "").toLowerCase();
+
+  console.log(newReservation);
+
+  reservations.push(newReservation);
+
+  res.json(newReservation);
+});
 
 app.listen(port, () => {
   console.log(`Server listening at port: ${port}`)
